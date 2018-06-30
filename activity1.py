e541d0d6286391d71eec443a9e1d272220f40676
nemesisxd <40229431+nemesisxd@users.noreply.github.com>
2018-06-30 17:12:24 +0800

    Activity 1

--- activity1.py
+++ activity1.py
@@ -2,5 +2,5 @@ bill = input('How much is your total bill? ')
 
 payment = input('How much is your payment? ')
 
-change = int(payment) - int(bill)
-print('Hi! Your change is {}'.format(change))
+change = float(payment) - float(bill)
+print('Hi! Your change is {:.2f}'.format(change))
